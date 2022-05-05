76109f374aff79ea00ea68af432ad5e5840a133e
Silverquent <dasilva.quentin33@gmail.com>
2022-05-05 17:13:54 +0200

    ok

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,15 @@
 import Head from 'next/head'
+import Portfolio from './Portfolio'
 
 export default function Home() {
   return (
     <body className="container">
       <Head>
         <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />        
-      </Head>
+        <link rel="icon" href="/favicon.ico" />
+        <link rel="stylesheet" href="/css/main.css" />
+      </Head>  
+      <Portfolio/>
     </body>
   )
 }
